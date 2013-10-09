97a045c1e942e3064a3a0bc656478b5e7b98e872
robertsambuena <robert@any.tv>
2013-10-09 11:21:27 +0800

    w

--- model/connection.php
+++ model/connection.php
@@ -1,15 +1,16 @@
 <?php
+
 $host="localhost"; // Host name 
-$username="root"; // Mysql username 
-$password=""; // Mysql password 
-$db_name="vidtracker_db"; // Database name 
+$username="anytv_robert"; // Mysql username 
+$password="=}D3]*LT3^%@2"; // Mysql password 
+$db_name="anytv_heartbeat_db"; // Database name 
 
 // Connect to server and select database.
 mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
 mysql_select_db("$db_name")or die("cannot select DB");
-// new mongo('mongodb://<dbuser>:<dbpassword>@ds041178.mongolab.com:41178/heartbeat_db)
 
-$dbname = 'yank_db';  
+$dbname = 'heartbeatdb';  
+
 $m = new MongoClient();
 $db = $m->selectDB($dbname);
-?>	
+?>
